3201d34bef18e531e7e3e8f4a1c89f84f402fea3
Zach Dahl <z.schtauffen@gmail.com>
2016-01-25 18:49:25 -0600

    set up to allow pathname m.route.mode

--- server.js
+++ server.js
@@ -4,4 +4,10 @@ var app = express()
 
 app.use(express.static("./"))
 
+app.use("*", (req, res, next) => {
+  res.send(
+    "<!doctype html><meta charset='utf-8'><title>Mithril-Redux</title><script defer src='/bundle.js'></script>"
+  )
+})
+
 app.listen(3000)
