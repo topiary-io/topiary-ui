56345a1fdc59aaadf20fd0a616dfdcec299bf114
Zach Dahl <z.schtauffen@gmail.com>
2016-01-31 22:15:03 -0600

    add .loader and /admin/loader for testing it

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(express.static("./"))
 
 app.use("*", (req, res, next) => {
   res.send(
-    "<!doctype html><meta charset='utf-8'><title>Mithril-Redux</title><link rel='stylesheet' href='/styles.css'><script defer src='/bundle.js'></script>"
+    "<!doctype html><meta charset='utf-8'><title>Mithril-Redux</title><link rel='stylesheet' href='/styles.css'><script defer src='/bundle.js'></script><div class='loader'>Loading...</div>"
   )
 })
 
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import m from "mithril"
 import App from "./containers/app"
 import SideBar from "./containers/side-bar"
+import Loader from "./containers/loader"
 
 const Login = {
   view:  () => m("p", "Login... (TODO)")
@@ -11,6 +12,7 @@ const routes = {
   "/admin/login": Login,
   "/admin": { // the app is only served at /admin/*
     "$container": SideBar,
+    "/loader": Loader,
     "/": {view: ()=>m("p", "home")},
     "$default": {view:()=>m("p", "404: page not found")}
   }
